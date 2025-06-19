0dc441bd3eccc2c2c50e671e959dcf73079c39b5
srikanthpragada <srikanthpragada@gmail.com>
2025-06-19 18:48:48 +0530

    19-JUN

---
+++ demo/src/basics/EvenNumbers.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class EvenNumbers {
+	public static void main(String[] args) {
+
+		for (int i = 2; i <= 50; i += 2)
+			System.out.printf("%d ", i);
+
+	}
+
+}
--- demo/src/basics/Numbers.java
+++ demo/src/basics/Numbers.java
@@ -3,8 +3,7 @@ package basics;
 public class Numbers {
 
 	public static void main(String[] args) {
- 
-       
+        
            for(int n = 1; n <= 10; n ++)
         	   System.out.printf("%d ", n);
            
